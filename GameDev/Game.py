048fe74a062251773d11dd31757a0c8200284662
NightDev19 <sherwintajan143@gmail.com>
2023-02-17 20:32:44 +0800

    Add existing file

--- GameDev/Game.py
+++ GameDev/Game.py
@@ -16,18 +16,9 @@ verts = (
 )
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7),
+    (0,1),(0,3),(0,4),(2,1),
+    (2,3),(2,7),(6,3),(6,4),
+    (6,7),(5,1),(5,4),(5,7),
 )
 
 surfaces = (
@@ -63,9 +54,6 @@ def Cube():
             glColor3fv(colors[x])
             glVertex3fv(verts[vertex])
     glEnd()
-    
-    
-    
     
     glBegin(GL_LINES)
     for edge in edges:
