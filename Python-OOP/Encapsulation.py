446d82aa8be5f533ce5d52e6fd953b9a65dbeea8
NightDev19 <sherwintajan143@gmail.com>
2023-09-16 21:50:23 +0800

    Add Python OOP

---
+++ Python-OOP/Encapsulation.py
@@ -0,0 +1,21 @@
+class Base:
+    def __init__(self):
+        self.a = "Sherwin"
+        self.__c = "Sherwin"
+ 
+# Creating a derived class
+class Derived(Base):
+    def __init__(self):
+ 
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self.__c)
+ 
+ 
+# Driver code
+obj1 = Base()
+print(obj1.a)
+# obj2 = Derived()
+# obj2()
